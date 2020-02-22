2e83a1a3e76b4935c4396854819390b0910cb817
Brokemia <rrwthird@aol.com>
2020-02-22 17:21:32 -0500

    Basic invisibility. Menu option needs work

--- TestMod/TestModule.cs
+++ TestMod/TestModule.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using Mod.Postman;
 using Mod.Postman.Module;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace TestMod {
     public class TestModule : PostmanModule {
+
+        GameObject invisOption;
+        bool playerInvis;
+        OptionScreen optionScreen;
+
         public override void Load() {
             Postman.Events.PlayerController.OnUpdate += PlayerController_OnUpdate;
+            On.OptionScreen.Init += OptionScreen_Init;
         }
 
+        void OptionScreen_Init(On.OptionScreen.orig_Init orig, OptionScreen self, IViewParams screenParams) {
+            optionScreen = self;
+            invisOption = UnityEngine.Object.Instantiate(self.fullscreenOption, self.backgroundFrame);
+            //UnityEngine.Object.Destroy(invisOption.GetComponentInChildren<Button>());
+            Button button = invisOption.GetComponentInChildren<Button>();
+            button.onClick = new Button.ButtonClickedEvent();
+            button.onClick.AddListener(OnInvis);
+            foreach(Component c in invisOption.GetComponentsInChildren<Component>()) {
+                Console.WriteLine("\n\n\n\n\n\n\n" + c.name + " " + c.GetType() + " " + c.gameObject.name + " " + c.gameObject.GetType());
+            }
+            orig(self, screenParams);
+        }
+
+        void OnInvis() {
+            playerInvis = !playerInvis;
+            //optionScreen.optionsChanged = true;
+            Console.WriteLine("Player Invisible: " + playerInvis);
+        }
 
         void PlayerController_OnUpdate(PlayerController controller) {
-            controller.GiveAirJump();
+            if(playerInvis) {
+                controller.Hide();
+            } else {
+                controller.Show();
+            }
         }
 
     }
