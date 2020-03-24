f6c57faa6bd6aa1b1f14a34116fb11463d319d28
Brokemia <rrwthird@aol.com>
2020-03-24 10:17:45 -0400

    Allowed external clearing on LeafGolemBoss handlers

--- Assembly-CSharp.Courier.mm/Patches/LeafGolemBoss.cs
+++ Assembly-CSharp.Courier.mm/Patches/LeafGolemBoss.cs
@@ -4,4 +4,39 @@ using MonoMod;
 public class patch_LeafGolemBoss : LeafGolemBoss {
     [MonoModPublic]
     private StateMachine stateMachine;
+
+    [MonoModIgnore]
+    public new event Action onDie;
+
+    [MonoModIgnore]
+    public new event Action onThrow;
+
+    [MonoModIgnore]
+    public new event Action onReceiveHit;
+
+    [MonoModIgnore]
+    public new event Action onSpawnInDone;
+
+    [MonoModIgnore]
+    public new event Action onIdleAnimStart;
+
+    public void ClearOnDieHandlers() {
+        onDie = null;
+    }
+
+    public void ClearOnThrowHandlers() {
+        onThrow = null;
+    }
+
+    public void ClearOnReceiveHitHandlers() {
+        onReceiveHit = null;
+    }
+
+    public void ClearOnSpawnInDoneHandlers() {
+        onSpawnInDone = null;
+    }
+
+    public void ClearOnIdleAnimStartHandlers() {
+        onIdleAnimStart = null;
+    }
 }
