b619f11c7623ddca062e3ef1b9e44bc8114fbe12
Brokemia <rrwthird@aol.com>
2020-06-05 02:30:39 -0400

    Maybe eventually I'll remember to update the version number with an update

--- Assembly-CSharp.Courier.mm/Courier/Courier.cs
+++ Assembly-CSharp.Courier.mm/Courier/Courier.cs
@@ -13,7 +13,7 @@ using UnityEngine;
 namespace Mod.Courier {
     public static partial class Courier {
 
-        public static Version CourierVersion = new Version(0, 6, 1);
+        public static Version CourierVersion = new Version(0, 6, 2);
 
         public static string CourierVersionString = "Courier v" + CourierVersion + "-alpha";
 
