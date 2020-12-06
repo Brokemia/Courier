b73eb6c0a078de4262e2b6004222e2ca4047cc1b
Brokemia <rrwthird@aol.com>
2020-12-05 20:48:45 -0500

    Updated version number

--- Assembly-CSharp.Courier.mm/Courier/Courier.cs
+++ Assembly-CSharp.Courier.mm/Courier/Courier.cs
@@ -14,7 +14,7 @@ using UnityEngine;
 namespace Mod.Courier {
     public static partial class Courier {
 
-        public static Version CourierVersion = new Version(0, 6, 7);
+        public static Version CourierVersion = new Version(0, 6, 8);
 
         public static string CourierVersionString = "Courier v" + CourierVersion + "-alpha";
 
